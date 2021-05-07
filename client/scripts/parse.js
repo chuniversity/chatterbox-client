1226a00a1e279bf4660b86634d5ba6ec54b157f8
Chuniversity <contact@chunis.org>
2021-05-07 14:24:36 -0400

    parse.js

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -2,18 +2,34 @@ var Parse = {
 
   server: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
 
-  create: function(message, successCB, errorCB = null) {
+  create: function (message, successCB, errorCB = null) {
     // todo: save a message to the server
+    // url
+    // type : post
+    // pretty much the same as below
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function (error) {
+        console.error('chatterbox: Failed to send message', error);
+      }
+    });
+
+
+
   },
 
-  readAll: function(successCB, errorCB = null) {
+  readAll: function (successCB, errorCB = null) {
     $.ajax({
       url: Parse.server,
       type: 'GET',
       data: { order: '-createdAt' },
       contentType: 'application/json',
       success: successCB,
-      error: errorCB || function(error) {
+      error: errorCB || function (error) {
         console.error('chatterbox: Failed to fetch messages', error);
       }
     });
